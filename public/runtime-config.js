3180919159938340034985c98a764864df0699ff
KevinSar <38804180+KevinSarav@users.noreply.github.com>
2026-07-16 05:16:58 +0000

    Regenerate runtime-config.js with correct Google Docs resume URLs

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,17 +1,13 @@
 window.__APP_CONFIG__ = {
-  appName: 'Website',
-  myName: 'REPLACE_ME',
-  role: 'REPLACE_ME',
+  appName: 'Kevin Saravia Showcase',
+  myName: 'Kevin Saravia',
+  role: 'Software Engineer',
   summary: 'REPLACE_ME',
   location: 'REPLACE_ME',
   availability: 'REPLACE_ME',
-  publicUrl: '',
-  resumeEmbedUrl: '',
-  resumeDownloadUrl: '',
+  publicUrl: 'kevin-saravia.shares.zrok.io',
+  resumeEmbedUrl: 'https://docs.google.com/document/d/1yMGiWCsvH42EaJzwUJZczI3rwDVF91DFgq_Sc1i1qj4/preview',
+  resumeDownloadUrl: 'https://docs.google.com/document/d/1yMGiWCsvH42EaJzwUJZczI3rwDVF91DFgq_Sc1i1qj4/export?format=pdf',
   note1: 'REPLACE_ME',
   note2: 'REPLACE_ME',
-}
-
-if (window.__APP_CONFIG__.appName) {
-  document.title = window.__APP_CONFIG__.appName
 }
